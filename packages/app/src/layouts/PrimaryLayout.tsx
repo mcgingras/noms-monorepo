dd37981f16f8e898c90f04979be7240d6d56589e
Michael Gingras <mcg79@cornell.edu>
2024-03-19 22:14:28 -0600

    Adds grid background

--- packages/app/src/layouts/PrimaryLayout.tsx
+++ packages/app/src/layouts/PrimaryLayout.tsx
@@ -1,6 +1,8 @@
 const PrimaryLayout = ({ children }: { children: React.ReactElement }) => {
   return (
-    <main className="h-screen max-w-screen-2xl mx-auto p-8">{children}</main>
+    <main className="h-screen bg-[url('/grid.svg')]">
+      <div className="max-w-screen-2xl mx-auto p-8">{children}</div>
+    </main>
   );
 };
 
