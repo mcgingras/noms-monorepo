497575f57308ce457b83ccbfd42b1cc409b98148
Michael Gingras <mcg79@cornell.edu>
2024-09-02 21:55:31 -0600

    Cleaning workspace

--- packages/contracts/src/Nom.sol
+++ packages/contracts/src/Nom.sol
@@ -1,14 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import { ERC721 } from "solady/tokens/ERC721.sol";
+
 contract Nom {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
+    constructor() {
+        // pass
     }
 }
