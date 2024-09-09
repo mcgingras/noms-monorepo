11d682f3c18da699c322d0e571ec7973fc4bb1ef
Michael Gingras <mcg79@cornell.edu>
2024-09-09 08:10:48 -0600

    Updates deployer

--- packages/contracts/script/TraitDeployer.s.sol
+++ packages/contracts/script/TraitDeployer.s.sol
@@ -9,13 +9,24 @@ import {INomTraits} from "../src/interfaces/INomTraits.sol";
 
 contract TraitDeployer is Script {
     IEasel public easel;
-    INomTraits public traits;
+    INomTraits public traitsContract;
     string public constant FILE_NAME = "traits.json";
     uint8 public constant PALETTE_INDEX = 0;
+    uint256 public constant BATCH_SIZE = 20;
+
+    struct Trait {
+        string filename;
+        bytes data;
+    }
 
     constructor(address _easel, address _traits) {
         easel = IEasel(_easel);
-        traits = INomTraits(_traits);
+        traitsContract = INomTraits(_traits);
+    }
+
+    function streamJsonFile(string memory filePath) internal view returns (string memory) {
+        string memory jsonContent = vm.readFile(filePath);
+        return jsonContent;
     }
 
     function readAndLogJson() public view {
@@ -27,7 +38,8 @@ contract TraitDeployer is Script {
 
     function addColorsToEasel() public {
        string memory filePath = string.concat(vm.projectRoot(), "/script/data/", FILE_NAME);
-        try vm.parseJsonStringArray(filePath, ".palette") returns (string[] memory colors) {
+       string memory jsonContent = vm.readFile(filePath);
+        try vm.parseJsonStringArray(jsonContent, ".palette") returns (string[] memory colors) {
             console2.log("Successfully parsed colors. Count:", colors.length);
             for (uint i = 0; i < colors.length; i += 50) {
                 uint end = i + 50 > colors.length ? colors.length : i + 50;
@@ -44,19 +56,27 @@ contract TraitDeployer is Script {
         }
     }
 
-    function addTraitsToContract(string memory category) public {
+   function addTraitsToContract(string memory category) public {
+        string memory filePath = string.concat(vm.projectRoot(), "/script/data/", FILE_NAME);
+        string memory jsonContent = vm.readFile(filePath);
+
         string memory path = string(abi.encodePacked(".images.", category));
-        (string[] memory filenames, bytes[] memory data) = abi.decode(
-            vm.parseJson(FILE_NAME, path),
-            (string[], bytes[])
-        );
-        for (uint i = 0; i < filenames.length; i++) {
-            traits.registerTrait(data[i], filenames[i]);
+        bytes memory parsedData = vm.parseJson(jsonContent, path);
+        (Trait[] memory traits) = abi.decode(parsedData, (Trait[]));
+
+        uint256 traitCount = traits.length;
+        console2.log("Total traits for category", category, ":", traitCount);
+
+        for (uint256 i = 0; i < traitCount; i += BATCH_SIZE) {
+            uint256 end = i + BATCH_SIZE > traitCount ? traitCount : i + BATCH_SIZE;
+            for (uint256 j = i; j < end; j++) {
+                traitsContract.registerTrait(traits[j].data, traits[j].filename);
+            }
         }
     }
 
     function uploadAll() public {
-        readAndLogJson();
+        // readAndLogJson();
         addColorsToEasel();
         addTraitsToContract("bodies");
         addTraitsToContract("accessories");
